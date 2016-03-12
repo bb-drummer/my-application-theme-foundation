5789f1a1509570fa44c31d42efeedbd9043267dc
Björn Bartels <bartels@dragon-projects.net>
2016-03-12 17:47:37 +0100

    update foundation source

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,7 +8,7 @@
  * @package		BB's Zend Framework 2 Components
  * @package		Theme 'Basic'
  * @author		Björn Bartels [dragon-projects.net] <info@dragon-projects.net>
- * @link		http://gitlab.dragon-projects.de:81/groups/themes
+ * @link		https://gitlab.bjoernbartels.earth/groups/themes
  * @license		http://www.apache.org/licenses/LICENSE-2.0 Apache License, Version 2.0
  * @copyright	copyright (c) 2016 Björn Bartels [dragon-projects.net] <info@dragon-projects.net>
  */
